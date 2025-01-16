ec42b4998ccbdd86cbab35f18bdb619a6f42d176
Paul-Nicolas Madelaine <pnm@pnm.tf>
2025-01-16 22:34:38 +0100

    update

--- nixos/configurations/default.nix
+++ nixos/configurations/default.nix
@@ -1,24 +1,23 @@
 let
   root = ../..;
   sources = import /${root}/sources;
-  lib = sources.nixpkgs.lib;
   modulesPath = /${root}/nixos/modules;
 in
-lib.mapAttrs
-  (
-    hostname: system:
-    lib.nixosSystem {
-      inherit system;
-      modules = [
-        ./${hostname}
-        { networking.hostName = hostname; }
-      ];
-      specialArgs = {
-        modules = lib.mapAttrs (m: _: /${modulesPath}/${m}) (builtins.readDir /${modulesPath});
-        inherit sources;
-      };
-    }
-  )
-  {
-    "mikhail" = "x86_64-linux";
+{
+  system ? builtins.currentSystem,
+  pkgs ? import sources.nixpkgs { inherit system; },
+  lib ? sources.nixpkgs.lib,
+}:
+lib.genAttrs [ "mikhail" ] (
+  hostname:
+  lib.nixosSystem {
+    inherit system;
+    modules = [
+      ./${hostname}
+    ];
+    specialArgs = {
+      modules = lib.mapAttrs (m: _: /${modulesPath}/${m}) (builtins.readDir /${modulesPath});
+      inherit sources;
+    };
   }
+)
